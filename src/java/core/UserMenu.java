60cdbc46e23df39bc8ba94e4f188bf596f85233e
RomanHalaburda <mtbrider94@gmail.com>
2017-11-02 13:49:40 +0300

    Start!

---
+++ src/java/core/UserMenu.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Roman
+ */
+@WebServlet(name = "UserMenu", urlPatterns = {"/UserMenu"})
+public class UserMenu extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    public String username;
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");        
+        username=request.getParameter("user"); // берем имя пользоватля
+        PrintWriter out=response.getWriter();
+        
+        try
+        {
+            session=request.getSession();
+            session.setAttribute("username",request.getParameter("user")); // передаем имя пользователя
+            String username = session.getAttribute("username").toString(); // берем имя пользоватля      
+         
+            // отображение чат-комнаты
+            out.println("<html> <head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"> <title>Chat Room</title>");
+            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\">\n");
+            out.println("<script type=\"text/javascript\" src=\"js/script.js\"></script>");
+            out.println("</head>");
+            out.println("<div class=\"clock\"> \n" +
+            "                <form name=\"form_clock\">\n" +
+            "                    <p><input id=\"inputdate\" name=\"date\" type=\"text\" name=\"date\" value=\"\" size=\"12\" disabled=\"true\"></p>\n" +
+            "                    <p><input id=\"inputtime\" name=\"time\" type=\"text\" name=\"time\" value=\"\" size=\"12\" disabled=\"true\"></p>\n" +
+            "                </form> \n" +
+            "           </div>");
+            out.println("<body onload=\"datetime()\">");
+            out.println("<div class=\"page-wrapper\"><center><h2>Привет, ");
+            out.println(username + "!");
+            out.println("<br>Добро пожаловать в WebChat");
+            out.println("</h2><br>");            
+            out.println("<form name=\"chatWindow\" action=\"chatWindow\">");
+            
+            out.println("<textarea  readonly=\"readonly\" id=\"text_message\" name=\"txtMessage\">");
+                  
+            // выбираем историю сообщений из базы данных        
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/choiser","root","root");              
+                Statement st = connection.createStatement();                 
+                ResultSet rs = st.executeQuery("select * from message;");
+            
+                // вывод истории сообщений
+                while(rs.next())
+                {
+                    String messages = rs.getString(2) + " >> " + rs.getString(3);
+                    out.println(messages);
+                }               
+                connection.close();
+            }
+            catch(Exception exception) 
+            {
+                System.err.println(exception.getMessage());
+            }
+            out.println("</textarea><br>");
+            out.println("<input placeholder=\"Ваше сообщение\" type=\"text\" id=\"txt_msg\" name=\"txtMsg\" value=\"\" /><input type=\"submit\" value=\"Отправить\" id=\"cmd_msg\" name=\"cmdSend\"/>");
+            out.println("</form>");
+            out.println("</div></body>");
+            out.println("</html>"); 
+        }
+        catch(Exception e)
+        {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Ошибка!</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1><br><br><center>Ошибка сервлета</center></h1>");
+            out.println("</body>");
+            out.println("</html>");
+            System.out.println(e);
+        }     
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    HttpSession session;
+}
---
+++ web/Welcome.jsp
@@ -0,0 +1,40 @@
+<%-- 
+    Document   : Welcome
+    Created on : 01.11.2017, 21:22:54
+    Author     : Maria
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Welcome - Choiser</title>        
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script type="text/javascript" src="js/script.js"></script>
+    </head>
+    <body onload="datetime()">
+        <div class="clock"> 
+                <form name="form_clock">
+                    <p><input id="inputdate" name="date" type="text" name="date" value="" size="14" disabled="true"></p>
+                    <p><input id="inputtime" name="time" type="text" name="time" value="" size="14" disabled="true"></p>
+                </form> 
+        </div>
+        <div class="page-wrapper">
+            <center>
+                <h1>Привествуем в Choiser!</h1>
+                <h1>Сервис для множественного голосования.</h1><br><br><br>
+                <h2>Авторизация</h2><br><br>
+                <form name="UserMenu" action="UserMenu" method="POST">
+                    <input placeholder="Логин" type="text" name="user" value="" />
+                    <br><br>
+                    <input placeholder="Пароль" type="text" name="password" value="" />
+                    <br><br>
+                    <input type="submit" value="Авторизоваться" name="login" />
+                    <br><br>
+                    <input type="checkbox" value="admin" name="isadmin" /> Я администратор
+                </form>
+            </center>
+        </div>
+    </body>
+</html>
